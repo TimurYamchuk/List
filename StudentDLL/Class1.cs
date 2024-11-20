5ffec2bc2af8c1eda2127c1a6724f639e80b10a7
yamch <yamch@DESKTOP-UL6T4JC>
2024-11-21 00:03:32 +0200

    Добавьте файлы проекта.

---
+++ StudentDLL/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonLibrary;
+
+namespace StudentLibrary
+{
+    public class Student : Person, IComparable<Student>
+    {
+        public double Average { get; set; }
+        public int GroupNumber { get; set; }
+
+        public Student() : base() { }
+
+        public Student(string name, string surname, int age, string phone, double average, int groupNumber)
+            : base(name, surname, phone, age)
+        {
+            Average = average;
+            GroupNumber = groupNumber;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Средний балл: {Average}\nНомер группы: {GroupNumber}");
+        }
+
+        public int CompareTo(Student other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public class AgeComparer : IComparer<Student>
+        {
+            public int Compare(Student x, Student y)
+            {
+                if (x == null || y == null)
+                    throw new ArgumentNullException("Один из объектов для сравнения равен null");
+
+                return x.Age.CompareTo(y.Age);
+            }
+        }
+
+        public class SurnameComparer : IComparer<Student>
+        {
+            public int Compare(Student x, Student y)
+            {
+                if (x == null || y == null)
+                    throw new ArgumentNullException("Один из объектов для сравнения равен null");
+
+                return string.Compare(x.Surname, y.Surname, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
